b00f966a4f87cc9075e658814583515d037a3b09
Никита Пашин <nikita.pashin.programming@gmail.com>
2023-07-19 20:07:32 +0700

    add fileLoader, add svgLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -28,8 +28,24 @@ export const buildLoaders = ({ isDev }: BuildOptions): webpack.RuleSetRule[] => 
     exclude: /node_modules/,
   };
 
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+      },
+    ],
+  };
+
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  };
+
   return [
     cssLoader,
+    fileLoader,
+    svgLoader,
     typescriptLoader,
   ];
 };

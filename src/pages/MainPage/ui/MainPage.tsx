88f85e920b243388c3ebeddb99d9affb4e359851
Никита Пашин <nikita.pashin.programming@gmail.com>
2023-07-19 21:15:03 +0700

    add i18n

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,18 +1,21 @@
+import { useTranslation } from 'react-i18next';
 import { useTheme } from '../../../app/providers/ThemeProvider/index';
 import { RoutePath } from '../../../shared/config/routeConfig/routeConfig';
 import { AppLink } from '../../../shared/ui';
 
 const MainPage = () => {
+  const { t, i18n } = useTranslation();
   const { toggleTheme } = useTheme();
   
   const onClick = () => {
     toggleTheme();
+    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
   };
 
   return (
     <div>
       <AppLink to={RoutePath.profile}>Профиль</AppLink>
-      <button onClick={onClick} type="button">Тема</button>
+      <button onClick={onClick} type="button">{t('theme')}</button>
       <p />
       <div>123 dawd awd awdawd aWDUIOAhfi UOawFI UHawifH awhf Lakwhf OKawh</div>
       <div>123 dawd awd awdawd aWDUIOAhfi UOawFI UHawifH awhf Lakwhf OKawh</div>

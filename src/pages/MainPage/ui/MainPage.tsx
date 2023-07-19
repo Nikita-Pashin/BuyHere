0004b7a5acb76e60dd8d7a15ff3d051ca6be2376
Никита Пашин <nikita.pashin.programming@gmail.com>
2023-07-19 22:45:46 +0700

    add eslint

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,13 @@
+import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '../../../app/providers/ThemeProvider/index';
 import { RoutePath } from '../../../shared/config/routeConfig/routeConfig';
 import { AppLink } from '../../../shared/ui';
 
-const MainPage = () => {
+const MainPage: FC = () => {
   const { t, i18n } = useTranslation();
   const { toggleTheme } = useTheme();
-  
+
   const onClick = () => {
     toggleTheme();
     i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');

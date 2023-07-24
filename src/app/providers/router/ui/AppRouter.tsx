6e20eedf8aaae9a762b387a7a7761e101f3adccd
Никита Пашин <nikita.pashin.programming@gmail.com>
2023-07-24 20:13:13 +0700

    fix CI

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,34 +1,31 @@
 import { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from '../../../../shared/config/routeConfig/routeConfig';
 
-export const AppRouter = () => {
-  const { t } = useTranslation();
-
-  return (
-    <Suspense
-      fallback={
-        <div>{t('loading')}</div>
-      }
-    >
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={(
-              <Suspense
-                fallback={
-                  <div>{t('loading')}</div>
-                }
-              >
-                {element}
-              </Suspense>
-            )}
-          />
-        ))}
-      </Routes>
-    </Suspense>
-  );
-};
+export const AppRouter = () => (
+  <Suspense
+    fallback={
+      // eslint-disable-next-line
+      <div>Loading...</div>
+    }
+  >
+    <Routes>
+      {Object.values(routeConfig).map(({ element, path }) => (
+        <Route
+          key={path}
+          path={path}
+          element={(
+            <Suspense
+              fallback={
+                // eslint-disable-next-line
+                <div>Loading...</div>
+              }
+            >
+              {element}
+            </Suspense>
+          )}
+        />
+      ))}
+    </Routes>
+  </Suspense>
+);
